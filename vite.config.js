5f8ce47115950aa07ec340662fd25e5c3a9f3897
Nouran Ashraf <123274078+Nourmeena@users.noreply.github.com>
2025-04-07 00:23:00 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/weather_last_app/", // Must match your GitHub repo name exactly
   plugins: [react()],
-})
+});
